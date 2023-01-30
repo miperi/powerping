858f9d140f05eccbf365e9264fb0f92da8a1de78
miperi <40628887+miperi@users.noreply.github.com>
2023-01-31 01:01:23 +0200

    Add files via upload

---
+++ Untitled-1.ps1
@@ -0,0 +1,257 @@
+if (!(Test-Path -path "C:\LDPorts")) {New-Item "C:\LDPorts" -Type Directory}
+
+#   Set date
+
+$datestring = (Get-Date).ToString("s").Replace(":",".")
+
+function Test-Port{  
+
+[cmdletbinding(  
+    DefaultParameterSetName = '',  
+    ConfirmImpact = 'low'  
+)]  
+    Param(  
+        [Parameter(  
+            Mandatory = $True,  
+            Position = 0,  
+            ParameterSetName = '',  
+            ValueFromPipeline = $True)]  
+            [array]$computer,  
+        [Parameter(  
+            Position = 1,  
+            Mandatory = $True,  
+            ParameterSetName = '')]  
+            [array]$port,  
+        [Parameter(  
+            Mandatory = $False,  
+            ParameterSetName = '')]  
+            [int]$TCPtimeout=1000,  
+        [Parameter(  
+            Mandatory = $False,  
+            ParameterSetName = '')]  
+            [int]$UDPtimeout=1000,             
+        [Parameter(  
+            Mandatory = $False,  
+            ParameterSetName = '')]  
+            [switch]$TCP,  
+        [Parameter(  
+            Mandatory = $False,  
+            ParameterSetName = '')]  
+            [switch]$UDP                                    
+        )  
+    Begin {  
+        If (!$tcp -AND !$udp) {$tcp = $True}  
+        #Typically you never do this, but in this case I felt it was for the benefit of the function  
+        #as any errors will be noted in the output of the report          
+        $ErrorActionPreference = "SilentlyContinue"  
+        $report = @()
+        $report | Format-List
+    }  
+    Process {     
+        ForEach ($c in $computer) {  
+            ForEach ($p in $port) {  
+                If ($tcp) {    
+                    #Create temporary holder $temp = "" | Select Server, Port, TypePort, Open, Notes   
+                    $temp = "" | Select Port, Open
+                    #Create object for connecting to port on computer  
+                    $tcpobject = new-Object system.Net.Sockets.TcpClient  
+                    #Connect to remote machine's port                
+                    $connect = $tcpobject.BeginConnect($c,$p,$null,$null)  
+                    #Configure a timeout before quitting  
+                    $wait = $connect.AsyncWaitHandle.WaitOne($TCPtimeout,$false)  
+                    #If timeout  
+                    If(!$wait) {  
+                        #Close connection  
+                        $tcpobject.Close()  
+                        Write-Verbose "Connection Timeout"  
+                        #Build report  
+                        #$temp.Server = $c
+                        $temp.Port = $p  
+                        #$temp.TypePort = "TCP" 
+                        $temp.Open = "Closed or filtered"  
+                        #$temp.Notes = "Connection to Port Timed Out"  
+                    } Else {  
+                        $error.Clear()  
+                        $tcpobject.EndConnect($connect) | out-Null  
+                        #If error  
+                        If($error[0]){  
+                            #Begin making error more readable in report  
+                            [string]$string = ($error[0].exception).message  
+                            $message = (($string.split(":")[1]).replace('"',"")).TrimStart()  
+                            $failed = $true  
+                        }  
+                        #Close connection      
+                        $tcpobject.Close()  
+                        #If unable to query port to due failure  
+                        If($failed){  
+                            #Build report   
+                            #$temp.Server = $c
+                            $temp.Port = $p   
+                            #$temp.TypePort = "TCP" 
+                            $temp.Open = "Closed or filtered"  
+                            #$temp.Notes = "$message"  
+                        } Else{  
+                            #Build report    
+                            #$temp.Server = $c
+                            $temp.Port = $p 
+                            #$temp.TypePort = "TCP"   
+                            $temp.Open = "Open"    
+                        }  
+                    }     
+                    #Reset failed value  
+                    $failed = $Null      
+                    #Merge temp array with report              
+                    $report += $temp  
+                }      
+                If ($udp) {  
+                    #Create temporary holder $temp = "" | Select Server, Port, TypePort, Open, Notes   
+                    $temp = "" | Select Port, Open                                    
+                    #Create object for connecting to port on computer  
+                    $udpobject = new-Object system.Net.Sockets.Udpclient
+                    #Set a timeout on receiving message 
+                    $udpobject.client.ReceiveTimeout = $UDPTimeout 
+                    #Connect to remote machine's port                
+                    Write-Verbose "Making UDP connection to remote server" 
+                    $udpobject.Connect("$c",$p) 
+                    #Sends a message to the host to which you have connected. 
+                    Write-Verbose "Sending message to remote host" 
+                    $a = new-object system.text.asciiencoding 
+                    $byte = $a.GetBytes("$(Get-Date)") 
+                    [void]$udpobject.Send($byte,$byte.length) 
+                    #IPEndPoint object will allow us to read datagrams sent from any source.  
+                    Write-Verbose "Creating remote endpoint" 
+                    $remoteendpoint = New-Object system.net.ipendpoint([system.net.ipaddress]::Any,0) 
+                    Try { 
+                        #Blocks until a message returns on this socket from a remote host. 
+                        Write-Verbose "Waiting for message return" 
+                        $receivebytes = $udpobject.Receive([ref]$remoteendpoint) 
+                        [string]$returndata = $a.GetString($receivebytes)
+                        If ($returndata) {
+                           Write-Verbose "Connection Successful"  
+                            #Build report  
+                            #$temp.Server = $c
+                            $temp.Port = $p 
+                            #$temp.TypePort = "UDP"    
+                            $temp.Open = "Open"  
+                            #$temp.Notes = $returndata   
+                            $udpobject.close()   
+                        }                       
+                    } Catch { 
+                        If ($Error[0].ToString() -match "\bRespond after a period of time\b") { 
+                            #Close connection  
+                            $udpobject.Close()  
+                            #Make sure that the host is online and not a false positive that it is open 
+                            If (Test-Connection -comp $c -count 1 -quiet) { 
+                                Write-Verbose "Connection Open"  
+                                #Build report   
+                                #$temp.Server = $c
+                                $temp.Port = $p 
+                                #$temp.TypePort = "UDP" 
+                                $temp.Open = "Open"  
+                            } Else { 
+                                <# 
+                                It is possible that the host is not online or that the host is online,  
+                                but ICMP is blocked by a firewall and this port is actually open. 
+                                #> 
+                                Write-Verbose "Host maybe unavailable"  
+                                #Build report   
+                                #$temp.Server = $c
+                                $temp.Port = $p  
+                                #$temp.TypePort = "UDP"  
+                                $temp.Open = "Closed or filtered"  
+                                #$temp.Notes = "Unable to verify if port is open or if host is unavailable."                                 
+                            }                         
+                        } ElseIf ($Error[0].ToString() -match "forcibly closed by the remote host" ) { 
+                            #Close connection  
+                            $udpobject.Close()  
+                            Write-Verbose "Connection Timeout"  
+                            #Build report  
+                            #$temp.Server = $c
+                            $temp.Port = $p 
+                            #$temp.TypePort = "UDP"
+                            $temp.Open = "Closed or filtered"  
+                            #$temp.Notes = "Connection to Port Timed Out"                         
+                        } Else {                      
+                            $udpobject.close() 
+                        } 
+                    }     
+                    #Merge temp array with report              
+                    $report += $temp 
+                }                                  
+            }  
+        }                  
+    }  
+    End {  
+        #Generate Report
+        $report
+    }
+}
+
+function TCPtest()
+{
+
+Clear-Host
+Write-Host -ForegroundColor Yellow "`n`nTesting TCP ports used by LANDESK... (This operation can take up to 30sc)"
+$TCPresults = Test-Port -comp $ip -port 80,137,139,443,445,4343,44345,5007,9594,9595,9982,12175,12176,16992,16993,16994,33354 -tcp -TCPtimeout 1800
+$TCPresults | Out-File $TCPtxt
+Write-Host -ForegroundColor Yellow "`n`nReport has been created in C:\LDPorts\$ip\TCP"
+sleep -Seconds 3
+explorer.exe "C:\LDPorts\$ip"
+
+}
+
+function UDPtest()
+{
+
+Clear-Host
+Write-Host -ForegroundColor Yellow "`n`nTesting UDP ports used by LANDESK... (This operation can take up to 15sc)"
+$UDPresults = Test-Port -comp $ip -port 53 -udp -UDPtimeout 1800
+$UDPresults | Out-File $UDPtxt
+Write-Host -ForegroundColor Yellow "`n`nReport has been created in C:\LDPorts\$ip\UDP"
+sleep -Seconds 3
+explorer.exe "C:\LDPorts\$ip"
+
+}
+
+function Fulltest()
+{
+
+Clear-Host
+Write-Host -ForegroundColor Yellow "`n`nTesting TCP and UDP ports used by LANDESK... (This operation can take up to 45sc)"
+$TCPresults = Test-Port -comp $ip -port 80,137,139,443,445,4343,44345,5007,9594,9595,9982,12175,12176,16992,16993,16994,33354 -tcp -TCPtimeout 1800
+$TCPresults | Out-File $TCPtxt
+$UDPresults = Test-Port -comp $ip -port 67,69,1759,4011,9535,9595,38293 -udp -UDPtimeout 1800
+$UDPresults | Out-File $UDPtxt
+Write-Host -ForegroundColor Yellow "`n`nReports have been created in C:\LDPorts\$ip\UDP and C:\LDPorts\$ip\TCP"
+sleep -Seconds 4
+explorer.exe "C:\LDPorts\$ip"
+
+}
+
+Clear-Host
+$ip = Read-Host "`n`nPlease enter the hostname or the IP address of the Core Server, or the CSA/Tunnel"
+
+Clear-Host
+Write-Host "`n`nDo you want to check for TCP or UDP ports?"
+Write-Host "`n`n`t1. TCP"
+Write-Host "`n`t2. UDP"
+Write-Host "`n`t3. TCP and UDP"
+$Protocol = Read-Host "`n`nPlease enter your choice number"
+
+Clear-Host
+
+if (!(Test-Path -path "C:\LDPorts\$ip")) {New-Item "C:\LDPorts\$ip" -Type Directory}
+if (!(Test-Path -path "C:\LDPorts\$ip\UDP")) {New-Item "C:\LDPorts\$ip\UDP" -Type Directory}
+if (!(Test-Path -path "C:\LDPorts\$ip\TCP")) {New-Item "C:\LDPorts\$ip\TCP" -Type Directory}
+
+$TCPtxt = "C:\LDPorts\$ip\TCP\TCPportsCheck-$ip-$datestring.txt"
+$UDPtxt = "C:\LDPorts\$ip\UDP\UDPportsCheck-$ip-$datestring.txt"
+
+switch ($Protocol)
+        {
+        1{TCPtest}
+        2{UDPtest}
+        3{Fulltest}
+        }
+
+
---
+++ aiv1.ps1
@@ -0,0 +1,119 @@
+###### csv configuration ###
+# 1. fields= comment,ip,port,email
+# 1.1 if the line start with # = ignore this line
+# 2. format on ports = 80:tcp
+# 3. format for many ports = 80:tcp|53:tcp
+# 4. do not sent email (only on total report) if the email = noemail
+
+
+
+# Configurable variables
+$csvFile = "C:\Users\miper\code\IPs.csv" # Path to the CSV file containing the IPs 
+$timeout = 1000 # Maximum milliseeconds time to wait for the port tests
+$mailfrom = "testing@testing.gr"
+$smtpserveris = "5.5.5.5"
+$primaryemailto = "michalis.perivolaris@hd.gr"
+
+#Operations
+##reset log file
+#New-Item -Path $logFile -ItemType File -Force -ErrorAction SilentlyContinue
+
+#Functions
+## write log file of what the script doing
+Function Write-Log {
+    param(
+        [string]$logText,
+        [string]$logFileis
+    )
+    $logText = "$(Get-Date -Format s) `t $logText"
+    Add-Content -Path $logFileis -Value $logText 
+}
+## Sent email
+Function Mailing {
+    param(
+        [string]$tois,
+        [string]$subjectis,
+        [string]$bodyis
+    )
+    #$Whenhappen= " $(Get-Date -Format s) "
+    if($ip.email -eq "noemail") {
+        write-host "ALERT email - but the email is --noemail--"
+    }
+    else{
+    #Send-MailMessage -To $tois -Subject $subjectis+$Whenhappen -Body $bodyis -from $mailfrom -SmtpServer $smtpserveris -ErrorAction SilentlyContinue
+    write-host "Email: -To $tois -Subject $subjectis -Body $bodyis -from $mailfrom -SmtpServer $smtpserveris -ErrorAction SilentlyContinue"
+    }
+}
+
+
+# Import the CSV file
+$ipList = Import-Csv -Path $csvFile
+$ipList = $ipList  | Where-Object { $_.comment -notmatch '^#' }
+
+# Create the log folder and log file
+$logFolder = "C:\Logs"
+$logFile = "$logFolder\IPcheck.log"
+$logFileAlert = "$logFolder\IPcheckAlert.log"
+New-Item -Path $logFolder -ItemType Directory -Force -ErrorAction SilentlyContinue
+New-Item -Path $logFileAlert -ItemType File -Force -ErrorAction SilentlyContinue
+
+# Start logging
+Write-Log -logText "INFO`tSystem`tStart testing round" -logFileis $logFile
+
+# Loop through the list of IPs
+foreach ($ip in $ipList) {
+    # Check the IP address (ping)
+    Write-Log -logText "INFO`t$($ip.IP)`t($($ip.comment)) Start testing" -logFileis $logFile
+    write-host -ForegroundColor Yellow "INFO`t$($ip.IP)`t($($ip.comment)) Start testing"
+    $pingResult = Test-Connection -ComputerName $ip.IP -Count 1  -Quiet -WarningAction SilentlyContinue 
+    if ($false -eq $pingResult) {
+        # If the ping times out, send the email alert
+        $pingalerttxt = "ALERT`t$($ip.IP)`t($($ip.comment)) Address is NOT reachable (ping)"
+        Write-Log -logText $pingalerttxt -logFileis $logFile
+        Write-Log -logText $pingalerttxt -logFileis $logFileAlert
+        write-host -ForegroundColor red $pingalerttxt
+        Mailing -tois $ip.email -subjectis $pingalerttxt -bodyis "The IP address $($ip.IP) is not reachable. Please check the connection."        # Log the ping timeout
+    }
+    else {
+        # If the ping does not time out, check the ports
+        Write-Log -logText "INFO`t$($ip.IP)`t($($ip.comment)) Address is reachable (ping)" -logFileis $logFile
+        Write-host "INFO`t$($ip.IP)`t($($ip.comment)) Address is reachable (ping)"
+        $ports = $ip.Port -split '\|'
+        foreach ($port in $ports) {
+            $portDetails = $port -split ':'
+            $tcpClient = [System.Net.Sockets.TcpClient]::new()
+            $portresult = $tcpClient.BeginConnect($ip.IP, $portDetails[0], $null, $null)
+            $success = $portresult.AsyncWaitHandle.WaitOne($timeout)
+            if ($success -eq $True) {
+                $tcpClient.EndConnect($result)
+                $tcpClient.Close()
+                Write-host -ForegroundColor Green "INFO`t$($ip.IP)`t($($ip.comment)) Port $($portDetails[0]) is reachable"
+                Write-Log -logText "INFO`t$($ip.IP)`t($($ip.comment)) Port $($portDetails[0]) is reachable" -logFileis $logFile
+            } else {
+                # If the port test fails, send the email alert
+                $portalerttxt = "ALERT`t$($ip.IP)`t($($ip.comment)) Port $($portDetails[0]) is NOT reachable"
+                $tcpClient.Close()
+                Write-Log -logText $portalerttxt -logFileis $logFile
+                Write-Log -logText $portalerttxt -logFileis $logFileAlert
+                Write-host -ForegroundColor Red $portalerttxt
+                Mailing -tois $ip.email -subjectis $portalerttxt  -bodyis "The port $($portDetails[0]) is not reachable on IP address $($ip.IP). Please check the connection." 
+            }
+        }
+    }
+}
+
+# End logging
+Write-Log -logText "INFO`tSystem`tTest round END" -logFileis $logFile
+
+# Alert with total report if something go wrong
+$countAlerts = ""
+$countAlerts = (Select-String -Path $logFileAlert -Pattern "ALERT" | Measure-Object).Count
+if ($null -ne $countAlerts) {
+    write-host -BackgroundColor Yellow "ALERT EVERYONE:"
+    $textalert = Get-Content $logFileAlert -Raw
+    Mailing -tois $primaryemailto -subjectis "ALERT $($countAlerts) from conn test" -bodyis $textalert
+}
+Else {
+    write-host -BackgroundColor Yellow "NO ALERTS - YEA!"
+}
+
